d29f2fa062eff7a0eee48668afa36db359cb58fd
AlecGrover <39810460+AlecGrover@users.noreply.github.com>
2024-01-28 13:42:24 -0800

    Persistence

---
+++ Assets/GamePersistence.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePersistence : MonoBehaviour
+{
+    public static GamePersistence instance;
+
+    public int DayCount;
+    public int GoldCount;
+    public List<Character> ActiveNPCs;
+    public List<Card> Deck;
+
+    public bool bHoldingSaveData = false;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void Save()
+    {
+        var player = FindObjectOfType<CardHolder>();
+        if (player != null)
+        {
+            Deck = player.GetAllOwnedCards();
+        }
+        var gameMode = FindObjectOfType<GameMode>();
+        if (gameMode != null)
+        {
+            gameMode.GetPersistenceData(out DayCount, out GoldCount);
+        }
+        bHoldingSaveData = true;
+    }
+
+    public void Load()
+    {
+        if (!bHoldingSaveData) return;
+        var player = FindObjectOfType<CardHolder>();
+        if (player != null)
+        {
+            player.SetDeck(Deck);
+        }
+        var gameMode = FindObjectOfType<GameMode>();
+        if (gameMode != null)
+        {
+            gameMode.SetPersistenceData(DayCount, GoldCount);
+        }
+    }
+
+}
